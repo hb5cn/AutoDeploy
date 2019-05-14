2807481f2ef44b2619bbac65f575d2aeb72b4203
zhangnan <hb5.cn@163.com>
2019-05-14 19:07:11 +0800

    完成修改配置文件部分，正在编写API接口。

--- AutoDeploy.py
+++ AutoDeploy.py
@@ -3,38 +3,236 @@
 
 import os
 import sys
-import socket
+import time
+import flask
+import logging
 import traceback
+
+app = flask.Flask(__name__)
 
 
 class AutoDeploy(object):
     def __init__(self):
         self.home_dir = os.path.split(os.path.realpath(__file__))[0]
 
-    def changeconfig(self, config_file, product_name):
+        # 初始化日志模块
+        self.logging = logging
+        # 定义工程的初始路径
+
+        # 定义日志文件输入的路径及文件名
+        logfile = '{:s}/LOG/AutoTestLog{:s}.log'.format(self.home_dir, time.strftime('%Y%m%d%H%M%S',
+                                                                                     time.localtime(time.time())))
+        self.logging.basicConfig(filename=logfile, level=self.logging.INFO,
+                                 format='%(asctime)s %(funcName)s %(lineno)d %(levelname)s %(message)s',
+                                 datefmt='%Y-%m-%d-%a %H:%M:%S', filemode='w')
+        # 设置一个屏幕打印的句柄
+        formatter = self.logging.Formatter('%(asctime)s %(funcName)s %(lineno)d %(levelname)s  %(message)s')
+        setlevel = self.logging.INFO
+        self.logscr = self.logging.StreamHandler()
+        self.logscr.setLevel(setlevel)
+        self.logscr.setFormatter(formatter)
+        self.mainlog = self.logging.getLogger('LOG')
+        self.mainlog.addHandler(self.logscr)
+
+    def changeconfig(self, config_file, product_name, svn_path, boss_path, billing_path, other_path, boss_config,
+                     billing_config, other_config, boss_changed, billing_changed, other_changed):
+        """
+        修改自动升级工具config里的内容。
+
+        :param config_file: config_auto_update_v1.9.1.cfg
+        :param product_name: api
+        :param svn_path: svn://123.57.180.25/dailyproduct/boss/20190514111130
+        :param boss_path: /opt/tomcat227_8083/webapps/
+        :param billing_path: /opt/tomcat227_8083/webapps/
+        :param other_path:
+        :param boss_config: svn://svn.uincall.com:3695/project/04_测试/99 部门管理/01 文档管理/10 自动化项目/
+        05 测试环境配置文件备份/101.200.111.227/227-8083/227-8083-bossautodeploy-xtboss-classes
+        :param billing_config: svn://svn.uincall.com:3695/project/04_测试/99 部门管理/01 文档管理/10 自动化项目/
+        05 测试环境配置文件备份/101.200.111.227/227-8083/227-8083-bossautodeploy-xtbilling-classes
+        :param other_config:
+        :param boss_changed: no
+        :param billing_changed: no
+        :param other_changed: no
+        :return:
+        """
         # config路径。
         config_path = os.path.join(self.home_dir, config_file)
 
         # 获取config文件原始内容。
         assert os.path.exists(config_path), '{0} is not exists.'.format(config_path)
-        fp = open(config_path, 'r')
+        fp = open(config_path, encoding='utf-8')
         config_content = fp.readlines()
         fp.close()
         newcontent = ''
 
-        # 将product_name进行替换
         for content in config_content:
+            if content.find('#') >= 0:
+                newcontent += content
+                continue
+
+            # 将product_name进行替换。
             if content.find('product_name=') >= 0:
-                print('Change Product : {0:s}'.format(product_name))
-                content = content.replace(content.split('=')[1], "'{0}'".format(str(product_name)))
+                self.mainlog.info('Change Product : {0:s}'.format(product_name))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(product_name))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), product_name)
+
+            # 将patchsvn_path=进行替换。
+            elif content.find('patchsvn_path=') >= 0:
+                self.mainlog.info('Change Svn_Path : {0:s}'.format(svn_path))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(svn_path))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), svn_path)
+
+            # 将install_path_xtboss=进行替换。
+            elif content.find('install_path_xtboss=') >= 0:
+                self.mainlog.info('Change install_path_xtboss : {0:s}'.format(boss_path))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(boss_path))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), boss_path)
+
+            # 将install_path_xtbilling=进行替换。
+            elif content.find('install_path_xtbilling=') >= 0:
+                self.mainlog.info('Change install_path_xtbilling : {0:s}'.format(billing_path))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(billing_path))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), billing_path)
+
+            # 将install_path_other=进行替换。
+            elif content.find('install_path_other=') >= 0:
+                self.mainlog.info('Change install_path_other : {0:s}'.format(other_path))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(other_path))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), other_path)
+
+            # 将config_svnpath_xtboss=进行替换。
+            elif content.find('config_svnpath_xtboss=') >= 0:
+                self.mainlog.info('Change config_svnpath_xtboss : {0:s}'.format(boss_config))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(boss_config))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), boss_config)
+
+            # 将config_svnpath_xtbilling=进行替换。
+            elif content.find('config_svnpath_xtbilling=') >= 0:
+                self.mainlog.info('Change config_svnpath_xtbilling : {0:s}'.format(billing_config))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(billing_config))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), billing_config)
+
+            # 将config_svnpath_other=进行替换。
+            elif content.find('config_svnpath_other=') >= 0:
+                self.mainlog.info('Change config_svnpath_other : {0:s}'.format(other_config))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(other_config))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), other_config)
+
+            # 将config_changed_xtboss=进行替换。
+            elif content.find('config_changed_xtboss=') >= 0:
+                self.mainlog.info('Change config_changed_xtboss : {0:s}'.format(boss_changed))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(boss_changed))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), boss_changed)
+
+            # 将config_changed_xtbilling=进行替换。
+            elif content.find('config_changed_xtbilling=') >= 0:
+                self.mainlog.info('Change config_changed_xtbilling : {0:s}'.format(billing_changed))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(billing_changed))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), billing_changed)
+
+            # 将config_changed_other=进行替换。
+            elif content.find('config_changed_other=') >= 0:
+                self.mainlog.info('Change config_changed_other : {0:s}'.format(other_changed))
+                source_str = content.split('=')[1].strip('\r\n')
+                try:
+                    assert source_str is not ''
+                    content = content.replace(source_str, "'{0:s}'".format(other_changed))
+                except AssertionError:
+                    content = "{:s}'{:s}'\n".format(content.strip('\r\n'), other_changed)
+
             newcontent += content
 
         # 将替换后的全部内容写入原文件
-        fp = open(config_path, 'w')
+        fp = open(config_path, 'w', encoding='utf-8')
         fp.write(newcontent)
         fp.close()
 
+    # def main(self):
+
+        # AutoDeployServer()
+        # self.changeconfig()
+        # self.changeconfig('config_auto_update_v1.9.1.cfg', 'api',
+        #                   'svn://123.57.180.25/dailyproduct/boss/20190514111130',
+        #                   '/opt/tomcat227_8083/webapps/', '/opt/tomcat227_8083/webapps/', '',
+        #                   'svn://svn.uincall.com:3695/project/04_测试/99 部门管理/01 文档管理/10 自动化项目/'
+        #                   '05 测试环境配置文件备份/101.200.111.227/227-8083/227-8083-bossautodeploy-xtboss-classes',
+        #                   'svn://svn.uincall.com:3695/project/04_测试/99 部门管理/01 文档管理/10 自动化项目/'
+        #                   '05 测试环境配置文件备份/101.200.111.227/227-8083/227-8083-bossautodeploy-xtbilling-classes', '',
+        #                   'no', 'no', 'no')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_parameters():
+    parameter_log = AutoDeploy().mainlog
+
+    config_file = flask.request.values.get('config_file')
+    parameter_log.info('config_file is : ', config_file)
+
+    product_name = flask.request.values.get('product_name')
+    parameter_log.info('product_name is : ', product_name)
+
+    svn_path = flask.request.values.get('svn_path')
+    parameter_log.info('svn_path is : ', svn_path)
+
+    boss_path = flask.request.values.get('boss_path')
+    parameter_log.info('boss_path is : ', boss_path)
+
+    boss_path = flask.request.values.get('boss_path')
+    parameter_log.info('boss_path is : ', boss_path)
+    return 'Success'
+
+
+app.run(port=40000)
 
 if __name__ == '__main__':
-    a = AutoDeploy()
-    a.changeconfig('config_auto_update_v1.9.1.cfg', 'api')
+    # a = AutoDeploy()
+    # a.changeconfig('config_auto_update_v1.9.1.cfg', 'api', 'svn://123.57.180.25/dailyproduct/boss/20190514111130',
+    #                '/opt/tomcat227_8083/webapps/', '/opt/tomcat227_8083/webapps/', '',
+    #                'svn://svn.uincall.com:3695/project/04_测试/99 部门管理/01 文档管理/10 自动化项目/05 测试环境配置文件备份/'
+    #                '101.200.111.227/227-8083/227-8083-bossautodeploy-xtboss-classes',
+    #                'svn://svn.uincall.com:3695/project/04_测试/99 部门管理/01 文档管理/10 自动化项目/05 测试环境配置文件备份/'
+    #                '101.200.111.227/227-8083/227-8083-bossautodeploy-xtbilling-classes', '', 'no', 'no', 'no')
+    # a.main()
+    pass
+
+
